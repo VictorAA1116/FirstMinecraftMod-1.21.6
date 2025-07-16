c3e4cf33ab241ff34c9491d571cb344c7db9142a
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2025-07-15 21:57:19 -0400

    Initial commit

---
+++ src/main/java/com/victor/custommod/CustomModClient.java
@@ -0,0 +1,10 @@
+package com.victor.custommod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/victor/custommod/CustomModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.victor.custommod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
